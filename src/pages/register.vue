75c700e4a2ad00d9796ef2da411656bdf9738158
sarasnt79 <sarasanati24@gmail.com>
2025-07-31 12:27:07 +0330

    change form

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,7 +1,21 @@
 <script setup></script>
 
 <template>
-  <div class="register flex flex-column p-[1rem]" style="width: 40%; height: 85%">
-    <h1 class="text-2xl font-bold w-full text-center">register form</h1>
+  <div class="register flex flex-col p-[1rem] w-full h-full items-center justify-center" >
+    <div class="register-form rounded-(--radius-12px) w-1/2 h-3/4 shadow-(--box-shadow-2)">
+      <div class="text-2xl font-bold w-full text-center grid grid-cols-2 h-full [border-radius:inherit]">
+        <div class="h-full [border-top-left-radius:inherit] [border-bottom-left-radius:inherit]" style="background-color: brown;">picture</div>
+
+        <div class="h-full [border-top-left-radius:inherit] [border-bottom-left-radius:inherit]">
+          <h1>REGISTER FORM</h1>
+          <div>
+            <input/>
+          </div>
+          <div>
+            <button>create account</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
